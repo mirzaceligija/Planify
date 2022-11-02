ec81af5200ca39127d37210aecfa730c83e06e50
mirzaceligija <mirza.celigija@outlook.com>
2022-11-02 22:12:25 +0100

    Added Global Exception handling, plus minor refactoring

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -1,21 +1,31 @@
+using API.Errors;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Planify.Application;
+using Planify.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  builder.Services.AddControllers();
+
+  builder.Services.AddEndpointsApiExplorer();
+  builder.Services.AddSwaggerGen();
+
+  builder.Services.AddApplication();
+  builder.Services.AddInfrastructure();
+
+  builder.Services.AddSingleton<ProblemDetailsFactory, PlanifyProblemDetailsFactory>();
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+  if (app.Environment.IsDevelopment())
+  {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+  }
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+  app.UseExceptionHandler("/error");
+  app.UseHttpsRedirection();
+  app.MapControllers();
+  app.Run();
+}
